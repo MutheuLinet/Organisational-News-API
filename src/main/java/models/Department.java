40ca39e9e9efbe6ebb32e2c2c02176c9cd7aebd7
MutheuLinet <muneenilinet@gmail.com>
2022-05-19 10:24:46 +0300

    Add Department Object properties, getters and setters

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,64 @@
+package models;
+
+import java.util.Objects;
+
+public class Department {
+    private int id;
+    private String name;
+    private  String description;
+    private int size; //number of employees
+
+    //LIST OF USERS
+
+    public Department(String name, String description, int size) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id && size == that.size && name.equals(that.name) && description.equals(that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, size);
+    }
+}
