420e8cf2f71a83ff7e6b697c3c7732d9547c4f41
MutheuLinet <muneenilinet@gmail.com>
2022-05-19 10:49:35 +0300

    Add News Interface Class with methods

---
+++ src/main/java/models/dao/NewsDao.java
@@ -0,0 +1,19 @@
+package models.dao;
+
+import models.News;
+
+import java.util.List;
+
+public interface NewsDao {
+    //create
+    void add(News news);
+
+    //read
+    List<News> allNews();
+    News findById(int id);
+    List<News> allNewsInDepartment(int department_id);
+
+
+
+
+}
