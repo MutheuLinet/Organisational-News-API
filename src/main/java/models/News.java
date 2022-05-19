f7f88d29c83c4f4f81a9c7ebf5778866656d6ba6
MutheuLinet <muneenilinet@gmail.com>
2022-05-19 10:47:37 +0300

    Add News Object properties, getters and setters

---
+++ src/main/java/models/News.java
@@ -0,0 +1,68 @@
+package models;
+
+import java.util.Objects;
+
+public class News {
+
+    private int id;
+    private String title;
+    private String content;
+    private int department_id;
+
+    public News(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public News(String title, String content, int department_id) {
+        this.title=title;
+        this.content=content;
+        this.department_id = department_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        News news = (News) o;
+        return id == news.id && department_id == news.department_id && title.equals(news.title) && content.equals(news.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content, department_id);
+    }
+
+}
